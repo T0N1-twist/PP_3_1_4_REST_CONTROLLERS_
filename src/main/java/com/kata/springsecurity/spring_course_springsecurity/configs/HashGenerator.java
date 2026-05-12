e5b246cd145105f38c788c042be9fbbffdfc943e
Tony Twist <tonitvist@gmail.com>
2026-05-12 21:56:39 +0300

    refactor: fix codestyle and delete class HashGenerator

--- src/main/java/com/kata/springsecurity/spring_course_springsecurity/configs/HashGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.kata.springsecurity.spring_course_springsecurity.configs;
-
-public class HashGenerator {
-    public static void main(String[] args) {
-        System.out.println(new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode("user"));
-
-   }
-}
